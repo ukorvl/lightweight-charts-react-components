bed190c611d4798e8c6637dedf42e45b608c5bbf
ukorvl <ukorotovskiy@gmail.com>
2025-05-13 13:41:38 +0400

    chore: add basic vitest setup

---
+++ lib/src/watermark/useWatermark.test.tsx
@@ -0,0 +1,5 @@
+import { describe, it } from "vitest";
+
+describe("useWatermark", () => {
+  it("should create a text watermark", () => {});
+});
--- lib/vitest.config.ts
+++ lib/vitest.config.ts
@@ -1,9 +1,17 @@
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     environment: "jsdom",
-    include: ["src/**/*.test.ts"],
+    include: ["src/**/*.test.{ts,tsx}"],
     globals: true,
+    coverage: {
+      provider: "v8",
+      reporter: ["json", "text", "lcov"],
+      include: ["src/**/*.{ts,tsx}"],
+      reportsDirectory: "coverage",
+    },
   },
 });
