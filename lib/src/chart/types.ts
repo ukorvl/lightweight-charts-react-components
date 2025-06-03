0ead559b33c89655e148e2f59b63ddf9024b426f
ukorvl <ukorotovskiy@gmail.com>
2025-06-04 00:28:34 +0400

    feat: add different chart types

--- lib/src/chart/types.ts
+++ lib/src/chart/types.ts
@@ -1,20 +1,13 @@
 import {
-  type ChartOptions as ChartNativeOptions,
+  type ChartOptions as TimeChartNativeOptions,
+  type YieldCurveChartOptions as YieldCurveChartNativeOptions,
+  type PriceChartOptions as PriceChartNativeOptions,
   type DeepPartial,
   type IChartApi,
   type MouseEventHandler,
   type Time,
 } from "lightweight-charts";
 import type { JSX, ReactNode } from "react";
-
-export type ChartProps = {
-  children?: ReactNode;
-  containerProps?: JSX.IntrinsicElements["div"];
-  onClick?: MouseEventHandler<Time>;
-  onCrosshairMove?: MouseEventHandler<Time>;
-  options?: DeepPartial<ChartNativeOptions>;
-  onDblClick?: MouseEventHandler<Time>;
-};
 
 export type ChartApiRef = {
   _chart: IChartApi | null;
@@ -28,10 +21,53 @@ export interface IChartContext {
   isReady: boolean;
 }
 
+type ChartCommonProps = {
+  children?: ReactNode;
+  containerProps?: JSX.IntrinsicElements["div"];
+};
+
+type TimeChartOwnProps = {
+  onClick?: MouseEventHandler<Time>;
+  onCrosshairMove?: MouseEventHandler<Time>;
+  onDblClick?: MouseEventHandler<Time>;
+  options?: DeepPartial<TimeChartNativeOptions>;
+};
+
+type YieldCurveChartProps = {
+  onClick?: MouseEventHandler<number>;
+  onCrosshairMove?: MouseEventHandler<number>;
+  onDblClick?: MouseEventHandler<number>;
+  options?: DeepPartial<YieldCurveChartNativeOptions>;
+};
+
+type OptionsChartOwnProps = {
+  onClick?: MouseEventHandler<number>;
+  onCrosshairMove?: MouseEventHandler<number>;
+  onDblClick?: MouseEventHandler<number>;
+  options?: DeepPartial<PriceChartNativeOptions>;
+};
+
+export type ChartOwnProps =
+  | TimeChartOwnProps
+  | YieldCurveChartProps
+  | OptionsChartOwnProps;
+
+export type ChartProps = ChartCommonProps & TimeChartOwnProps;
+export type YieldCurveChartOwnProps = ChartCommonProps & YieldCurveChartProps;
+export type OptionsChartProps = ChartCommonProps & OptionsChartOwnProps;
+
 export type ChartComponentProps = {
   container: HTMLElement;
 } & Omit<ChartProps, "containerProps">;
 
-export type UseChartOptions = {
+export type UseChartOptions<T extends ChartOwnProps> = {
   container: HTMLElement;
-} & Omit<ChartProps, "children" | "containerProps">;
+  createChartHandler: (
+    container: string | HTMLElement,
+    options?: DeepPartial<T["options"]>
+  ) => IChartApi;
+} & T;
+
+// so the chart api for different charts is also diferent - different type
+// can i handle it in 1 hook?
+// need to explore here more
