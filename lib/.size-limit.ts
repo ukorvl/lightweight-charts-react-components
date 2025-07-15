bd624e66cbd8d080879d7002d660083d71b92a91
ukorvl <u.korotovskiy@nil.foundation>
2025-07-15 19:05:53 +0400

    chore: check umd version size

--- lib/.size-limit.ts
+++ lib/.size-limit.ts
@@ -1,10 +1,18 @@
+import packageJson from "./package.json" with { type: "json" };
 import type { SizeLimitConfig } from "size-limit";
 
 export default [
   {
     name: "ESM",
-    path: "dist/lightweight-charts-react-components.mjs",
+    path: packageJson.module,
     limit: "3.5 kB",
+    import: "*",
+    brotli: true,
+  },
+  {
+    name: "UMD",
+    path: packageJson.unpkg,
+    limit: "8 kB",
     import: "*",
     brotli: true,
   },
