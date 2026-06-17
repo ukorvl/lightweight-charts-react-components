442fc4e9a33803d0e8f3f56a5efd5c7114c1cb9d
ukorvl <u.korotovskiy@nil.foundation>
2026-06-17 13:01:27 +0400

    fix: recreate watermark when type or src changes

--- lib/.size-limit.ts
+++ lib/.size-limit.ts
@@ -7,7 +7,7 @@ export default [
     // The limit should be little more than the actual size to avoid false positives due to minor changes in dependencies or build output
     // The purpose of this limit is to catch significant bundle size increases
     // It should be adjusted manually when new changes arrive
-    limit: "3.75 kB",
+    limit: "4 kB",
     import: "*",
     brotli: true,
   },
